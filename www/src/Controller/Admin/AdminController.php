a6bfc6f975129abdc59f68f005e5cbcdd246396a
ES_PREPA_DEV <setifeliott@gmail.com>
2026-02-09 11:15:51 +0100

    AdminController and Dashboard/Base views

---
+++ www/src/Controller/Admin/AdminController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use App\Repository\CriminalRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin')]
+#[IsGranted('ROLE_ADMIN')]
+final class AdminController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_dashboard')]
+    public function dashboard(UserRepository $userRepository, CriminalRepository $criminalRepository,): Response
+    {
+        //Tableau de stats global
+        $stats = [
+            'users' => [
+                'total' => $userRepository->count([]),
+                'active' => $userRepository->count(['isActive' => true]),
+                'admins' => count(array_filter($userRepository->findAll(), fn($u) => in_array('ROLE_ADMIN', $u->getRoles())))
+            ],
+            'criminals' => [
+                'total' => $criminalRepository->count([]),
+                'capture' => $criminalRepository->count(['isCaptured' => true]),
+            ]
+            //TODO: Add report section 
+        ];
+
+        //Récuperer les 5 criminels les plus récent
+        $recentCriminals = $criminalRepository->findBy(
+            [], //aucun filtre de tri
+            ['createdAt' => 'DESC'], // paramètre de tri 
+            5 //Limite les résultat a 5 
+        );
+
+        //Récupère les 5 derniers utilisateur inscrits
+        $recentUsers = $userRepository->findBy(
+            ['isActive' => true], //aucun filtre de tri
+            ['createdAt' => 'DESC'], // paramètre de tri 
+            5 //Limite les résultat a 5 
+        );
+
+        return $this->render('admin/dashboard.html.twig', [
+            'stats' => $stats,
+            'recentCriminals' => $recentCriminals,
+            'recentUsers' => $recentUsers
+        ]);
+    }
+}
